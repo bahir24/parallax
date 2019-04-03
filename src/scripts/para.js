a7ae233a81d811bab96def4bc83773ca0868ff03
Mikhail_Bakhirev <7074033@gmail.com>
2019-04-03 21:52:45 +0300

    smb

--- src/scripts/para.js
+++ src/scripts/para.js
@@ -1,20 +1,24 @@
 var para = (function () {
-    var bg = document.querySelector('.hero__bg');
+  var bg = document.querySelector('.hero__image');
     var user = document.querySelector('.hero__user');
     var head = document.querySelector('.hero__head');
 
     return {
         move: function (block, windowScroll, strafeAmount) {
             var strafe = windowScroll / -strafeAmount + '%';
-
             var style = block.style;
-
             style.top = strafe;
         },
 
         init: function(wScroll) {
           this.move(bg, wScroll, 45);
         }
+        // init: function(wScroll) {
+        //   this.move(head, wScroll, 75);
+        // };
+        // init: function(wScroll) {
+        //   this.move(user, wScroll, 105);
+        // };
     }
 
 }());
@@ -23,6 +27,6 @@ window.onscroll = function () {
   var wScroll = window.pageYOffset;
   
   para.init(wScroll);
-  console.log(wScroll);
+  // console.log(wScroll);
 
 }
