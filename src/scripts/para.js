9a6246070a2763edc5e0f1f5bda3d74ddf5ab218
bahir24 <7074033@gmail.com>
2019-04-03 17:59:44 +0300

    parallax_blur0.5

--- src/scripts/para.js
+++ src/scripts/para.js
@@ -1,19 +1,29 @@
 var para = (function () {
-    var bg = document.querySelector('.hero__bg');
+    var bg = document.querySelector('.hero__image');
     var user = document.querySelector('.hero__user');
     var head = document.querySelector('.hero__head');
 
     return {
         move: function (block, windowScroll, strafeAmount) {
             var strafe = windowScroll / -strafeAmount + '%';
+            var trString = 'translate3d(0, '+ strafe +', 0)';
+
+
+            // var strafe2 = strafe1 + '%';
+            // var strafe3 = strafe1 / 2 + '%';
+            // var trString = 'translate3d('+ strafe3 +', '+ strafe2 +', 0)';
 
             var style = block.style;
 
-            style.top = strafe;
+            style.transform = trString;
+            style.webkittransform = trString;
+            // style.top = strafe;
         },
 
         init: function(wScroll) {
           this.move(bg, wScroll, 45);
+          this.move(user, wScroll, 10);
+          this.move(head, wScroll, 30);
         }
     }
 
@@ -23,6 +33,5 @@ window.onscroll = function () {
   var wScroll = window.pageYOffset;
   
   para.init(wScroll);
-  console.log(wScroll);
 
 }
